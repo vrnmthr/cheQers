937f301bd4b93fc5f3139fd323eae4efe26b7c2f
MckennaCisler <mckennacisler@gmail.com>
2018-01-27 23:20:39 -0500

    Convert moves to linked lists; win fix

--- checkers/board.py
+++ checkers/board.py
@@ -7,10 +7,10 @@ class Board:
     """ A board, to be used as if always from the perspective of the white player
         (you should call board.flip() in order to player as the other player) """
 
-    def __init__(self, size, board_arr=None, start_player_num=0):
+    def __init__(self, size, board_arr=None, start_white_player=0):
         assert size == 8
         self.size = size
-        self.cur_player_num = start_player_num
+        self.cur_white_player = start_white_player
         if board_arr is not None:
             assert board_arr.shape == (size, size)
             self.board_arr = board_arr
@@ -20,10 +20,10 @@ class Board:
             self.setup_board()
 
     def __copy__(self):
-        return Board(self.size, copy.copy(self.board_arr), self.cur_player_num)
+        return Board(self.size, copy.copy(self.board_arr), self.cur_white_player)
 
     def __deepcopy__(self, memodict={}):
-        return Board(self.size, copy.deepcopy(self.board_arr, memodict), self.cur_player_num)
+        return Board(self.size, copy.deepcopy(self.board_arr, memodict), self.cur_white_player)
 
     def setup_board(self):
         """ Fills the board with pieces in their starting positions.
@@ -53,7 +53,8 @@ class Board:
         for coords in jumped_pieces:
             self.remove_piece(coords)
 
-        # and, move self piece (WE PRESUME that it's self piece) from its old spot (both on board and with the piece itself)
+        # and, move self piece (WE PRESUME that it's self piece) from its old spot
+        # (both on board and with the piece itself)
         self.move_piece(move_start, move_end)
 
     def available_white_moves(self):
@@ -76,8 +77,7 @@ class Board:
         """ Returns the winner of the game (player 1 or 2),
         0 for a stalemate, and -1 if the game is not finished """
 
-        movable_white_num = 0
-        movable_black_num = 0
+        movable_nums = [0, 0]
         for y in range(self.size):
             for x in range(self.size):
                 # make sure the piece exists, and if so sum movable pieces for each color)
@@ -86,30 +86,39 @@ class Board:
                     # only consider piece if it has possible moves
                     if len(Piece.get_all_possible_moves(self, coords)) > 0:
                         if Piece.is_white(self, coords):
-                            movable_white_num += 1
+                            movable_nums[self.cur_white_player] += 1
                         else:
-                            movable_black_num += 1
+                            movable_nums[(self.cur_white_player + 1) % 2] += 1
 
         # determine if anyone won (or if no one had any moves left)
-        if movable_white_num + movable_black_num == 0:
+        if movable_nums[0] + movable_nums[1] == 0:
             return 0
-        elif movable_black_num == 0:
-            return 1
-        elif movable_white_num == 0:
-            return 2
+        elif movable_nums[1] == 0:
+            return 1 # player 1 wins if player 2 has NO moves
+        elif movable_nums[0] == 0:
+            return 2 # player 2 wins if player 1 has NO moves
         else:
             return -1
+
+    def cur_player_won(self):
+        """ Returns 1 if the current player has won, 2 if the other has,
+        0 for a stalemate, and -1 if the game is not finished """
+        winner = self.winner()
+        if winner < 1:
+            return winner
+        else:
+            return 1 if self.cur_white_player == winner else 2
 
     def set_white_player(self, player_num):
         """ Sets the current "white" player by flipping the board if
             the current board doesn't match the provided player
             (note that the game starts with player 0 as white)"""
-        assert player_num >= 0 and player_num < 2
-        if self.cur_player_num % 2 != player_num:
-            self.flip()
-        self.cur_player_num = player_num
+        assert 0 <= player_num < 2
+        if self.cur_white_player % 2 != player_num:
+            self.__flip()
+        self.cur_white_player = player_num
 
-    def flip(self):
+    def __flip(self):
         """ Flips the board coordinates so that the other pieces are on top, AND
             converts all black players to white to completely swap perspective"""
         self.board_arr = np.rot90(self.board_arr, 2)
--- checkers/move.py
+++ checkers/move.py
@@ -1,14 +1,30 @@
 #!/usr/bin/python
 
 class Move:
-    def __init__(self, start, end, preceding_move, is_jump):
-        self.start = start
+    """ A single hop (the last known hop) of a linked list of moves and jumps. """
+    def __init__(self, last_hop, end, preceding_move, is_jump):
+        self.last_hop = last_hop
         self.end = end
-        self.preceding_move = preceding_move
         self.is_jump = is_jump
+        self.preceding_move = preceding_move
+        self.start = self.__find_start()
+
+    def __find_start(self):
+        # if this move is a jump with a preceding move,
+        # then our start is different from our last hop
+        if self.is_jump and self.preceding_move is not None:
+            move = self.preceding_move
+            while move.preceding_move is not None:
+                move = move.preceding_move
+            return move.start  # move for which preceding_move was None
+        else:
+            return self.last_hop
 
     def get_start(self):
         return self.start
+
+    def get_last_hop(self):
+        return self.last_hop
 
     def get_end(self):
         return self.end
@@ -22,20 +38,24 @@ class Move:
         if self.is_jump:
             # the piece this move is jumping should be between the start
             # and end of this move (the average of those two positions)
-            piece_x = (self.start[0] + self.end[0]) / 2;
-            piece_y = (self.start[1] + self.end[1]) / 2;
+            piece_x = (self.last_hop[0] + self.end[0]) / 2
+            piece_y = (self.last_hop[1] + self.end[1]) / 2
 
             # add this most recent jump...
-            pieces.append([piece_x, piece_y]);
+            pieces.append([piece_x, piece_y])
 
             # ...but also go back to get the inbetween ones (if we're not the first move)
             if self.preceding_move is not None:
                 prev_jumped = self.preceding_move.get_jumped_pieces(board)
-                pieces.extend(prev_jumped);
+                pieces.extend(prev_jumped)
 
-                # something is wrong (a preceding move isn't a jump) if this returns null, so let the error be thrown
+            # something is wrong (a preceding move isn't a jump) if this returns null, so let the error be thrown
 
         return pieces
 
     def __str__(self):
-        return "%s -> %s" % (self.start, self.end)
+        # build up chain from end
+        if self.preceding_move is not None:
+            return "%s -> %s" % (str(self.preceding_move), self.end)
+        else:
+            return "%s -> %s" % (str(self.start), str(self.end))
